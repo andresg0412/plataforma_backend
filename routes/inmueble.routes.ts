ac5b3fa2e584a0c43438e3baffb447984a04a575
German Andres Gamboa <andres.gamboa.9504@gmail.com>
2025-06-28 19:33:00 -0500

    fix: eliminar flujo inmuebles

--- routes/inmueble.routes.ts
+++
@@ -1,8 +0,0 @@
-import { FastifyInstance, FastifyPluginOptions } from 'fastify';
-import { inmuebleController } from '../controllers/inmueble.controller';
-import { authMiddleware } from '../middlewares/authMiddleware';
-
-export async function inmuebleRoutes(server: FastifyInstance, opts: FastifyPluginOptions) {
-  // GET /inmuebles - Listar inmuebles con filtros opcionales (requiere autenticación)
-  server.get('/', { preHandler: [authMiddleware] }, inmuebleController.list);
-}
